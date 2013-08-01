e80a2b5ec58b2c5a57c32fce1cc800ee600ec633
bvuongx <bvuong@gmail.com>
2013-08-01 14:17:44 -0700

    changed some layout things

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,7 +8,7 @@ class Trip < ActiveRecord::Base
   attr_accessible :endzipcode, :startzipcode
 
   def excuse
-    myarray = ['Kevin Spacey stole my bicycle','It was not in the original specification', 'I will have to fix that at a later date','It must be because of leap year','Godzilla pulled me over in a stolen cop car','A stunt double stole my identity', 'A British chap put me inside a Chinese finger trap', 'The entire Roman empire spin kicked my collar bone',  'Princess Peach beat me into submission', 'Scrooge McDuck hid my Trapper Keeper', 'Chuck E Cheese broke into my house', 'Raiden from Mortal Kombat tried to sell me vacuum cleaners',  'Eddie Murphy put a banana inside my tail pipe', 'My fake tarantuala kept telling me knock knock jokes', 'I swerved to avoid banana peels on the roadway' ]
+    myarray = ['Kevin Spacey stole my bicycle','It was not in the original specification', 'Godzilla pulled me over in a stolen cop car','A stunt double stole my identity', 'A British chap put me inside a Chinese finger trap', 'The entire Roman empire spin kicked my collar bone',  'Princess Peach beat me into submission', 'Scrooge McDuck hid my Trapper Keeper', 'Chuck E Cheese broke into my house', 'Raiden from Mortal Kombat tried to sell me vacuum cleaners',  'Eddie Murphy put a banana inside my tail pipe', 'My fake tarantuala kept telling me knock knock jokes', 'I swerved to avoid banana peels on the roadway' ]
     cover_story = myarray[rand(myarray.length)]
     return cover_story
   end
